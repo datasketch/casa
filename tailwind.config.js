d0aa31d84039084d1fbf392fe519511f7bf881f5
David Daza <03dazal@gmail.com>
2021-02-04 09:18:44 -0500

    change layout

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        ubuntu: ['Ubuntu', 'sans-serif'],
+        'ibm-plex-sans': ['"IBM Plex Sans"', 'sans-serif']
+      },
+      colors: {
+        charcoal: '#293845',
+        'eerie-black': '#222526',
+        'barbie-pink': '#da1c95',
+        'soft-white': '#fcfcfc',
+        cultured: '#f1f5f7'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
